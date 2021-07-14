b8de08f7354e6052a1b9747e7aeb72c75be6fd33
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-07-14 14:11:24 +0300

    Update Http session configuration

--- admin-gateway/src/main/java/eu/opertusmundi/admin/web/config/HttpSessionConfiguration.java
+++ admin-gateway/src/main/java/eu/opertusmundi/admin/web/config/HttpSessionConfiguration.java
@@ -2,7 +2,7 @@ package eu.opertusmundi.admin.web.config;
 
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
-@EnableJdbcHttpSession(tableName = "web.spring_session")
+@EnableJdbcHttpSession(tableName = "admin.spring_session")
 public class HttpSessionConfiguration {
 
 }
