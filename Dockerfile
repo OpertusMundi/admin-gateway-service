860047d4c7184e73cd56687e2f5377e842824ef9
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2021-07-20 13:49:14 +0300

    Add Docker recipe

---
+++ Dockerfile
@@ -0,0 +1,87 @@
+# vim: set syntax=dockerfile:
+
+FROM node:10.16.3-buster AS npm-build
+
+ENV NPM_CONFIG_PROGRESS="false" NPM_CONFIG_SPIN="false"
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+
+WORKDIR /app
+
+COPY admin-gateway/src/main/frontend/ /app/
+RUN npm install && npm run build
+
+
+#FROM maven:3.6.3-openjdk-8 as maven-build
+# see https://github.com/OpertusMundi/java-commons/blob/master/Dockerfile
+FROM opertusmundi/java-commons-builder:1.0.54 as maven-build
+
+WORKDIR /app
+
+COPY common /app/common/
+RUN (cd /app/common && mvn -B install)
+
+COPY pom.xml /app/
+COPY admin-gateway/pom.xml /app/admin-gateway/
+RUN mvn -B dependency:resolve-plugins dependency:resolve
+RUN mvn -B -pl admin-gateway dependency:copy-dependencies -DincludeScope=runtime
+
+COPY admin-gateway/src/main/resources /app/admin-gateway/src/main/resources
+COPY admin-gateway/src/main/java /app/admin-gateway/src/main/java
+COPY admin-gateway/resources /app/admin-gateway/resources
+COPY --from=npm-build /app/build /app/admin-gateway/src/main/frontend/build
+RUN mvn -B compile -DenableJavaBuildProfile -DenableDockerBuildProfile
+
+
+FROM openjdk:8-jre-alpine
+
+COPY --from=maven-build /app/admin-gateway/target/ /app/
+
+RUN addgroup spring && adduser -H -D -G spring spring
+
+COPY docker-entrypoint.sh /usr/local/bin
+RUN chmod a+x /usr/local/bin/docker-entrypoint.sh
+
+WORKDIR /app
+
+RUN mkdir config logs \
+    && chgrp spring config logs \
+    && chmod g=rwx config logs
+
+ENV MARKETPLACE_URL="" \
+    ADMIN_PASSWORD_FILE="/secrets/admin-password" \
+    DATABASE_URL="jdbc:postgresql://db:5432/opertusmundi" \
+    DATABASE_USERNAME="spring" \
+    DATABASE_PASSWORD_FILE="/secrets/database-password" \
+    CAMUNDA_DATABASE_URL="jdbc:postgresql://db:5432/opertusmundi-camunda" \
+    CAMUNDA_DATABASE_USERNAME="opertusmundi-camunda-reader" \
+    CAMUNDA_DATABASE_PASSWORD_FILE="/secrets/camunda-database-password" \
+    JWT_SECRET_FILE="/secrets/jwt-signing-key" \
+    BPM_REST_BASE_URL="http://bpm-server:8000/engine-rest" \
+    BPM_REST_USERNAME="" \
+    BPM_REST_PASSWORD_FILE="/secrets/bpm-rest-password" \
+    MANGOPAY_BASE_URL="https://api.mangopay.com" \
+    MANGOPAY_CLIENT_ID="" \
+    MANGOPAY_CLIENT_PASSWORD_FILE="/secrets/mangopay-client-password" \
+    CATALOGUE_BASE_URL="http://catalogueapi:8000/" \
+    INGEST_BASE_URL="http://ingest:8000/" \
+    TRANSFORM_BASE_URL="http://transform:8000/" \
+    MAILER_BASE_URL="http://mailer:8000/" \
+    MESSENGER_BASE_URL="http://messenger:8000/" \
+    RATING_BASE_URL="http://rating:8000/" \
+    RATING_USERNAME="" \
+    RATING_PASSWORD_FILE="/secrets/rating-password" \
+    PROFILE_BASE_URL="http://profile:8000/" \
+    PID_BASE_URL="http://pid:8000/" \
+    ELASTICSEARCH_BASE_URL="http://elasticsearch:9200" \
+    ELASTICSEARCH_INDICES_ASSETS_INDEX_NAME="assets"
+
+VOLUME [ \
+    "/var/local/opertusmundi/files/assets", \
+    "/var/local/opertusmundi/files/users", \
+    "/var/local/opertusmundi/files/drafts", \
+    "/var/local/opertusmundi/files/temp", \
+    "/var/local/opertusmundi/ingest/input" ]
+
+USER spring
+ENTRYPOINT [ "/usr/local/bin/docker-entrypoint.sh" ]
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+set -u -e -o pipefail
+
+[[ "${DEBUG:-false}" != "false" || "${XTRACE:-false}" != "false" ]] && set -x
+
+function _validate_http_url()
+{
+    local var_name=$1
+    local re="^\(https\|http\)://\([a-z][-a-z0-9]*\)\([.][a-z][-a-z0-9]*\)*\([:][1-9][0-9]\{1,4\}\)\?\(/\|$\)"
+    grep -e "${re}" || { echo "${var_name} does not seem like an http(s) URL" 1>&2 && false; }
+}
+
+function _validate_database_url()
+{
+    local var_name=$1
+    local re="^jdbc:postgresql://\([a-z][-a-z0-9]*\)\([.][a-z][-a-z0-9]*\)*\([:][1-9][0-9]\{1,4\}\)\?/[a-z][-_a-zA-Z0-9]*$"
+    grep -e "${re}" || { echo "${var_name} does not seem like a PostgreSQL JDBC connection URL" 1>&2 && false; }
+}
+
+runtime_profile=$(hostname | md5sum | head -c10)
+
+{
+    marketplace_url=$(echo ${MARKETPLACE_URL} | _validate_http_url "MARKETPLACE_URL")
+    echo "opertusmundi.marketplace.url = ${marketplace_url}"
+	
+	admin_password=$(cat ${ADMIN_PASSWORD_FILE} | tr -d '\n')
+	echo "opertus-mundi.default-admin.password = ${admin_password}"
+
+    database_url=$(echo ${DATABASE_URL} | _validate_database_url "DATABASE_URL")
+    database_username=${DATABASE_USERNAME}
+    database_password=$(cat ${DATABASE_PASSWORD_FILE} | tr -d '\n')
+    echo "spring.datasource.url = ${database_url}"
+    echo "spring.datasource.username = ${database_username}"
+    echo "spring.datasource.password = ${database_password}"
+    
+    camunda_database_url=$(echo ${CAMUNDA_DATABASE_URL} | _validate_database_url "CAMUNDA_DATABASE_URL")
+    camunda_database_username=${CAMUNDA_DATABASE_USERNAME}
+    camunda_database_password=$(cat ${CAMUNDA_DATABASE_PASSWORD_FILE} | tr -d '\n')
+    echo "opertusmundi.camunda.datasource.url = ${camunda_database_url}"
+    echo "opertusmundi.camunda.datasource.username = ${camunda_database_username}"
+    echo "opertusmundi.camunda.datasource.password = ${camunda_database_password}"
+
+    jwt_secret=$(cat ${JWT_SECRET_FILE} | tr -d '\n')
+    echo "opertusmundi.feign.jwt.secret = ${jwt_secret}"
+
+    bpm_rest_base_url=$(echo ${BPM_REST_BASE_URL} | _validate_http_url "BPM_REST_BASE_URL")
+    bpm_rest_username=${BPM_REST_USERNAME}
+    bpm_rest_password=$(cat ${BPM_REST_PASSWORD_FILE} | tr -d '\n')
+    echo "opertusmundi.feign.bpm-server.url = ${bpm_rest_base_url}"
+    echo "opertusmundi.feign.bpm-server.basic-auth.username = ${bpm_rest_username}"
+    echo "opertusmundi.feign.bpm-server.basic-auth.password = ${bpm_rest_password}"
+
+    mangopay_base_url=$(echo ${MANGOPAY_BASE_URL} | _validate_http_url "MANGOPAY_BASE_URL")
+    mangopay_client_id=${MANGOPAY_CLIENT_ID}
+    mangopay_client_password=$(cat ${MANGOPAY_CLIENT_PASSWORD_FILE} | tr -d '\n')
+    echo "opertusmundi.payments.mangopay.base-url = ${mangopay_base_url}"
+    echo "opertusmundi.payments.mangopay.client-id = ${mangopay_client_id}"
+    echo "opertusmundi.payments.mangopay.client-password = ${mangopay_client_password}"
+
+    catalogue_base_url=$(echo ${CATALOGUE_BASE_URL} | _validate_http_url "CATALOGUE_BASE_URL")
+    echo "opertusmundi.feign.catalogue.url = ${catalogue_base_url}"
+
+    ingest_base_url=$(echo ${INGEST_BASE_URL} | _validate_http_url "INGEST_BASE_URL")
+    echo "opertusmundi.feign.ingest.url = ${ingest_base_url}"
+
+    transform_base_url=$(echo ${TRANSFORM_BASE_URL} | _validate_http_url "TRANSFORM_BASE_URL")
+    echo "opertusmundi.feign.transform.url = ${transform_base_url}"
+
+    mailer_base_url=$(echo ${MAILER_BASE_URL} | _validate_http_url "MAILER_BASE_URL")
+    echo "opertusmundi.feign.email-service.url = ${mailer_base_url}"
+    echo "opertusmundi.feign.email-service.jwt.subject = api-gateway"
+
+    messenger_base_url=$(echo ${MESSENGER_BASE_URL} | _validate_http_url "MESSENGER_BASE_URL")
+    echo "opertusmundi.feign.message-service.url = ${messenger_base_url}"
+    echo "opertusmundi.feign.message-service.jwt.subject = api-gateway"
+
+    rating_base_url=$(test -z "${RATING_BASE_URL:-}" && echo -n || \
+        { echo ${RATING_BASE_URL} | _validate_http_url "RATING_BASE_URL"; })
+    rating_username=${RATING_USERNAME}
+    rating_password=$({ test -f "${RATING_PASSWORD_FILE}" && cat ${RATING_PASSWORD_FILE}; } | tr -d '\n' || echo -n)
+    echo "opertusmundi.feign.rating-service.url = ${rating_base_url}"
+    echo "opertusmundi.feign.rating-service.basic-auth.username = ${rating_username}"
+    echo "opertusmundi.feign.rating-service.basic-auth.password = ${rating_password}"
+
+    profile_base_url=$(echo ${PROFILE_BASE_URL} | _validate_http_url "PROFILE_BASE_URL")
+    echo "opertusmundi.feign.data-profiler.url = ${profile_base_url}"
+    
+    pid_base_url=$(echo ${PID_BASE_URL} | _validate_http_url "PID_BASE_URL")
+    echo "opertusmundi.feign.persistent-identifier-service.url= ${pid_base_url}"
+
+    elasticsearch_base_url=$(echo ${ELASTICSEARCH_BASE_URL%/} | _validate_http_url "ELASTICSEARCH_BASE_URL")
+    elasticsearch_indices_assets_index_name=${ELASTICSEARCH_INDICES_ASSETS_INDEX_NAME}
+    echo "spring.elasticsearch.rest.uris = ${elasticsearch_base_url}"
+    echo "opertusmundi.elastic.asset-index.name = ${elasticsearch_indices_assets_index_name}"
+
+} > ./config/application-${runtime_profile}.properties
+
+logging_config="classpath:config/log4j2.xml"
+if [[ -f "./config/log4j2.xml" ]]; then
+    logging_config="file:config/log4j2.xml"
+fi
+
+# Run
+
+main_class=eu.opertusmundi.admin.web.Application
+default_java_opts="-server -Djava.security.egd=file:///dev/urandom -Xms256m"
+exec java ${JAVA_OPTS:-${default_java_opts}} -cp "/app/classes:/app/dependency/*" ${main_class} \
+  --spring.profiles.active=production,${runtime_profile} --logging.config=${logging_config}
+
