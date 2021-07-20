6d5b83e994f623898bba320c5b0086873533e08e
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-07-20 12:50:12 +0300

    Review code for contracts

--- admin-gateway/src/main/java/eu/opertusmundi/admin/web/controller/action/ContractController.java
+++ admin-gateway/src/main/java/eu/opertusmundi/admin/web/controller/action/ContractController.java
@@ -1,56 +1,182 @@
 package eu.opertusmundi.admin.web.controller.action;
 
-import java.util.List;
+import java.util.Set;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
+import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import eu.opertusmundi.common.model.contract.MasterContractDraftDto;
-import eu.opertusmundi.common.model.contract.MasterContractDto;
+import eu.opertusmundi.common.model.EnumSortingOrder;
+import eu.opertusmundi.common.model.PageResultDto;
 import eu.opertusmundi.common.model.RestResponse;
+import eu.opertusmundi.common.model.contract.EnumContractStatus;
+import eu.opertusmundi.common.model.contract.helpdesk.EnumMasterContractSortField;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractCommandDto;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractDto;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractHistoryDto;
 
+@RequestMapping(value = "/action/contract", produces = MediaType.APPLICATION_JSON_VALUE)
 public interface ContractController {
 
-	@GetMapping(value = { "/action/user/contracts/{id}" })
-	RestResponse<MasterContractDto> findContract(@PathVariable int id);
+    /**
+     * Get all contracts
+     * 
+     * @param page
+     * @param size
+     * @param orderBy
+     * @param order
+     * @return
+     */
+    @GetMapping(value = {"/history"})
+    RestResponse<PageResultDto<MasterContractHistoryDto>> findAllHistory(
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "25") @Max(50) @Min(1) int size,
+        @RequestParam(name = "title", required = false) String title,
+        @RequestParam(name = "status", required = false) Set<EnumContractStatus> status,
+        @RequestParam(name = "orderBy", defaultValue = "MODIFIED_ON") EnumMasterContractSortField orderBy,
+        @RequestParam(name = "order", defaultValue = "DESC") EnumSortingOrder order
+    );
+    
+    /**
+     * Get all contract templates
+     * 
+     * @param page
+     * @param size
+     * @param orderBy
+     * @param order
+     * @return
+     */
+    @GetMapping(value = {"/templates"})
+    RestResponse<PageResultDto<MasterContractDto>> findAll(
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "25") @Max(50) @Min(1) int size,
+        @RequestParam(name = "orderBy", defaultValue = "MODIFIED_ON") EnumMasterContractSortField orderBy,
+        @RequestParam(name = "order", defaultValue = "DESC") EnumSortingOrder order
+    );
+    
+    /**
+     * Get a contract template by id
+     * 
+     * @param id
+     * @return
+     */
+    @GetMapping(value = {"/templates/{id}"})
+	RestResponse<?> findOne(
+        @PathVariable int id
+    );
 
-	@GetMapping(value = { "/action/user/contracts/drafts/{id}" })
-	RestResponse<MasterContractDraftDto> findDraft(@PathVariable int id);
-	
-	@GetMapping(value = { "/action/user/getContracts/" })
-	RestResponse<List<MasterContractDto>> getContracts();
+    /**
+     * Creates a new draft from an existing template.
+     * 
+     * If a draft already exists, the existing record is returned
+     * 
+     * @param id
+     * @return
+     */
+    @PostMapping(value = {"/history/{id}"})
+    RestResponse<MasterContractDto> createDraftForTemplate(
+        @PathVariable int id
+    );
+    
+    /**
+     * Deactivate contract template
+     * 
+     * @param id
+     * @return
+     */
+    @DeleteMapping(value = {"/history/{id}"})
+    RestResponse<MasterContractHistoryDto> deactivate(
+        @PathVariable int id
+    );
+    
+	/**
+	 * Get all contract drafts
+	 * 
+	 * @param page
+	 * @param size
+	 * @param orderBy
+	 * @param order
+	 * @return
+	 */
+    @GetMapping(value = {"/drafts"})
+    RestResponse<PageResultDto<MasterContractDto>> findAllDrafts(
+        @RequestParam(name = "page", defaultValue = "0") int page,
+        @RequestParam(name = "size", defaultValue = "25") @Max(50) @Min(1) int size,
+        @RequestParam(name = "orderBy", defaultValue = "MODIFIED_ON") EnumMasterContractSortField orderBy,
+        @RequestParam(name = "order", defaultValue = "DESC") EnumSortingOrder order
+    );
 
-	@GetMapping(value = { "/action/user/getDrafts/" })
-	RestResponse<List<MasterContractDraftDto>> getDrafts();
-	
-	@PostMapping(value = "/action/user/contracts")
-	RestResponse<MasterContractDraftDto> createDraft(
-		@Valid @RequestBody MasterContractDraftDto contract, BindingResult validationResult
-	);
+    /**
+     * Get a contract draft by id
+     * 
+     * @param id
+     * @return
+     */
+    @GetMapping(value = {"/drafts/{id}"})
+    RestResponse<MasterContractDto> findOneDraft(
+        @PathVariable int id
+    );
 
-	@PostMapping(value = { "/action/user/contracts/{id}" })
-	RestResponse<MasterContractDto> updateContract(
-		@PathVariable int id, @Valid @RequestBody MasterContractDto contract, BindingResult validationResult
-	);
-	
-	@PostMapping(value = { "/action/user/contracts/drafts/{id}" })
-	RestResponse<MasterContractDraftDto> updateDraft(
-		@PathVariable int id, @Valid @RequestBody MasterContractDraftDto contract, BindingResult validationResult
-	);
+	/**
+	 * Create new contract draft
+	 * 
+	 * @param contract
+	 * @param validationResult
+	 * @return
+	 */
+    @PostMapping(value = {"/drafts"})
+    RestResponse<MasterContractDto> createDraft(
+        @Valid @RequestBody MasterContractCommandDto command, 
+        BindingResult validationResult
+    );
 
-	@DeleteMapping(value = { "/action/user/contracts/{id}" })
-	RestResponse<Void> delete(@PathVariable int id);
-	
-	@DeleteMapping(value = { "/action/user/contracts/drafts/{id}" })
-	RestResponse<Void> deleteDraft(@PathVariable int id);
-	
-	@PostMapping(value = { "/action/user/contracts/updateState/{id}" })
-	RestResponse<Void> updateState(@PathVariable int id, @RequestBody String state);
+    /**
+     * Update existing contract draft
+     * 
+     * @param id
+     * @param contract
+     * @param validationResult
+     * @return
+     */
+    @PostMapping(value = {"/drafts/{id}"})
+    RestResponse<MasterContractDto> updateDraft(
+        @PathVariable int id, 
+        @Valid @RequestBody MasterContractCommandDto contract,
+        BindingResult validationResult
+    );
+    
+    /**
+     * Delete contract draft
+     * 
+     * @param id
+     * @return
+     */
+    @DeleteMapping(value = {"/drafts/{id}"})
+    RestResponse<MasterContractDto> deleteDraft(
+        @PathVariable int id
+    );
+
+    /**
+     * Publish contract draft
+     * 
+     * @param id
+     * @param state
+     * @return
+     */
+    @PutMapping(value = {"/drafts/{id}"})
+    RestResponse<MasterContractDto> publishDraft(
+        @PathVariable int id
+    );
 
 }
--- admin-gateway/src/main/java/eu/opertusmundi/admin/web/controller/action/ContractControllerImpl.java
+++ admin-gateway/src/main/java/eu/opertusmundi/admin/web/controller/action/ContractControllerImpl.java
@@ -1,343 +1,184 @@
 package eu.opertusmundi.admin.web.controller.action;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
-import eu.opertusmundi.common.domain.HelpdeskAccountEntity;
-import eu.opertusmundi.common.domain.MasterContractDraftEntity;
-import eu.opertusmundi.common.domain.MasterContractEntity;
-import eu.opertusmundi.common.domain.MasterContractHistoryEntity;
-import eu.opertusmundi.common.domain.MasterSectionDraftEntity;
-import eu.opertusmundi.common.domain.MasterSectionEntity;
-import eu.opertusmundi.common.domain.MasterSectionHistoryEntity;
+import eu.opertusmundi.common.model.ApplicationException;
+import eu.opertusmundi.common.model.EnumSortingOrder;
+import eu.opertusmundi.common.model.PageResultDto;
 import eu.opertusmundi.common.model.RestResponse;
-import eu.opertusmundi.common.model.account.helpdesk.HelpdeskAccountDto;
-import eu.opertusmundi.common.model.contract.MasterContractDraftDto;
-import eu.opertusmundi.common.model.contract.MasterContractDto;
-import eu.opertusmundi.common.model.contract.MasterContractHistoryDto;
-import eu.opertusmundi.common.model.contract.MasterSectionDraftDto;
-import eu.opertusmundi.common.model.contract.MasterSectionDto;
-import eu.opertusmundi.common.repository.HelpdeskAccountRepository;
-import eu.opertusmundi.common.repository.contract.MasterContractDraftRepository;
-import eu.opertusmundi.common.repository.contract.MasterContractHistoryRepository;
-import eu.opertusmundi.common.repository.contract.MasterContractRepository;
-import eu.opertusmundi.common.repository.contract.MasterSectionDraftRepository;
-import eu.opertusmundi.common.repository.contract.MasterSectionHistoryRepository;
-import eu.opertusmundi.common.repository.contract.MasterSectionRepository;
+import eu.opertusmundi.common.model.contract.EnumContractStatus;
+import eu.opertusmundi.common.model.contract.helpdesk.EnumMasterContractSortField;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractCommandDto;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractDto;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractHistoryDto;
+import eu.opertusmundi.common.model.contract.helpdesk.MasterContractQueryDto;
+import eu.opertusmundi.common.service.contract.MasterTemplateContractService;
 
-@Transactional(readOnly = false)
 @RestController
-@Secured({ "ROLE_ADMIN", "ROLE_USER" })
+@Secured({"ROLE_ADMIN", "ROLE_USER"})
 public class ContractControllerImpl extends BaseController implements ContractController {
+   
+    @Autowired
+    private MasterTemplateContractService masterService;
 
-	//private List<String> sortableFields = Arrays.asList("name");
+    @Override
+    public RestResponse<PageResultDto<MasterContractHistoryDto>> findAllHistory(
+        int page,
+        int size,
+        String title,
+        Set<EnumContractStatus> status,
+        EnumMasterContractSortField orderBy,
+        EnumSortingOrder order
+    ) {
+        final MasterContractQueryDto query = MasterContractQueryDto.builder()
+            .page(page)
+            .size(size)
+            .title(title)
+            .status(status)
+            .orderBy(orderBy)
+            .order(order)
+            .build();
 
-	@Autowired
-	private MasterContractRepository contractRepository;
+        final PageResultDto<MasterContractHistoryDto> result = masterService.findAllHistory(query);
 
-	@Autowired
-	private MasterContractDraftRepository contractDraftRepository;
-	
-	@Autowired
-	private MasterContractHistoryRepository contractHistoryRepository;
-	
-	@Autowired
-	private MasterSectionRepository sectionRepository;
+        return RestResponse.result(result);
+    }
+    
+    @Override
+    public RestResponse<PageResultDto<MasterContractDto>> findAll(
+        int page,
+        int size,
+        EnumMasterContractSortField orderBy,
+        EnumSortingOrder order
+    ) {
+        final MasterContractQueryDto query = MasterContractQueryDto.builder()
+            .page(page)
+            .size(size)
+            .orderBy(orderBy)
+            .order(order)
+            .build();
 
-	@Autowired
-	private MasterSectionDraftRepository sectionDraftRepository;
-	
-	@Autowired
-	private MasterSectionHistoryRepository sectionHistoryRepository;
+        final PageResultDto<MasterContractDto> result = masterService.findAll(query);
 
-	@Autowired
-	private HelpdeskAccountRepository helpdeskAccountRepo;
-	
+        return RestResponse.result(result);
+    }
 
-	@Override
-	public RestResponse<MasterContractDto> findContract(int id) {
-		final MasterContractEntity e = contractRepository.findById(id).orElse(null);
+    @Override
+    public RestResponse<?> findOne(int id) {
+        final MasterContractDto result = this.masterService.findOneById(id).orElse(null);
 
-		if (e == null) {
-			return RestResponse.notFound();
-		}
-		List<MasterSectionEntity> sections = contractRepository.findSectionsByContract(e);
-		List<MasterSectionDto> sectionsDto = new ArrayList<MasterSectionDto>() ;
-		for(MasterSectionEntity s : sections) sectionsDto.add(s.toDto());
-		MasterContractDto contractDto = e.toDto();
-		contractDto.setSections(sectionsDto);
-		return RestResponse.result(contractDto);
-	}
-	
-	@Override
-	public RestResponse<MasterContractDraftDto> findDraft(int id) {
-		final MasterContractDraftEntity e = contractDraftRepository.findById(id).orElse(null);
+        if (result == null) {
+            return RestResponse.notFound();
+        }
 
-		if (e == null) {
-			return RestResponse.notFound();
-		}
-		List<MasterSectionDraftEntity> sections = contractDraftRepository.findSectionsByContract(e);
-		List<MasterSectionDraftDto> sectionsDto = new ArrayList<MasterSectionDraftDto>() ;
-		for(MasterSectionDraftEntity s : sections) sectionsDto.add(s.toDto());
-		MasterContractDraftDto contractDto = e.toDto();
-		contractDto.setSections(sectionsDto);
-		return RestResponse.result(contractDto);
-	}
-	
-	@Override
-	public RestResponse<List<MasterContractDto>> getContracts(){
-		
-		final Optional<HelpdeskAccountEntity> accountEntity = helpdeskAccountRepo.findOneByEmail(this.currentUserName());
-		final List<MasterContractEntity> contracts = contractRepository.findContractsByAccount(accountEntity.get());
-		final List<MasterContractDto> contractsDto = new ArrayList<MasterContractDto>() ;
-		for(MasterContractEntity e : contracts) {
-			contractsDto.add(e.toDto());
-	}
-		return RestResponse.result(contractsDto);
-	}
-	
-	@Override
-	public RestResponse<List<MasterContractDraftDto>> getDrafts(){
-		
-		final Optional<HelpdeskAccountEntity> accountEntity = helpdeskAccountRepo.findOneByEmail(this.currentUserName());
-		final List<MasterContractDraftEntity> contracts = contractDraftRepository.findContractsByAccount(accountEntity.get());
-		final List<MasterContractDraftDto> contractsDto = new ArrayList<MasterContractDraftDto>() ;
-		for(MasterContractDraftEntity e : contracts) contractsDto.add(e.toDto());
-		return RestResponse.result(contractsDto);
-	}
+        return RestResponse.result(result);
+    }
+    
+    @Override
+    public RestResponse<MasterContractDto> createDraftForTemplate(int id) {
+        try {
+            final MasterContractDto result = this.masterService.createForTemplate(this.currentUserId(), id);
 
+            return RestResponse.result(result);
+        } catch (final ApplicationException ex) {
+            return RestResponse.error(ex.getCode(), ex.getMessage());
+        }
+    }
 
-	@Override
-	public RestResponse<MasterContractDraftDto> createDraft(MasterContractDraftDto record, BindingResult validationResult) {
-		if (validationResult.hasErrors()) {
-			return RestResponse.invalid(validationResult.getFieldErrors());
-		}
-		
-		//final Optional<AccountEntity> accountEntity = accountRepo.findOneByUsername(this.currentUserName());
+    @Override
+    public RestResponse<MasterContractHistoryDto> deactivate(int id) {
+        try {
+            final MasterContractHistoryDto result = this.masterService.deactivate(id);
 
-		// Retrieve entity from repository
-		final HelpdeskAccountDto account = helpdeskAccountRepo.findOneByEmail(this.currentUserName()).get().toDto();
+            return RestResponse.result(result);
+        } catch (final ApplicationException ex) {
+            return RestResponse.error(ex.getCode(), ex.getMessage());
+        }
+    }
 
-		record.setAccount(account);
-		List<MasterSectionDraftDto> sections = record.getSections();
-		record.setSections(null);
-		MasterContractDraftDto resultRecord = contractDraftRepository.saveFrom(record);
-		// create sections
-		final MasterContractDraftEntity e = contractDraftRepository.findById(resultRecord.getId()).get();
-		record.setId(e.getId());
-		record.setParentId(e.getId());
-		contractDraftRepository.saveFrom(record);
-		//List<SectionEntity> contractSections = contractRepository.findSectionsByContract(e.getId());
-		for (MasterSectionDraftDto s : sections){
-			s.setContract(record);
-			sectionDraftRepository.saveFrom(s);
-		}
-		
-		return RestResponse.result(resultRecord);
-	}
+    @Override
+    public RestResponse<PageResultDto<MasterContractDto>> findAllDrafts(
+        int page,
+        int size,
+        EnumMasterContractSortField orderBy,
+        EnumSortingOrder order
+    ) {
+        final PageResultDto<MasterContractDto> result = this.masterService.findAllDrafts(page, size, orderBy, order);
 
-	@Override
-	public RestResponse<MasterContractDto> updateContract(int id, MasterContractDto record, BindingResult validationResult) {
-		
-		final HelpdeskAccountDto account = helpdeskAccountRepo.findOneByEmail(this.currentUserName()).get().toDto();
+        return RestResponse.result(result);       
+    }
 
-		record.setAccount(account);
-		List<MasterSectionDto> sections = record.getSections();
-		record.setSections(null);
-		MasterContractDto resultRecord = contractRepository.saveFrom(record);
-		
+    @Override
+    public RestResponse<MasterContractDto> findOneDraft(int id) {
+        final MasterContractDto result = this.masterService.findOneDraft(id);
 
-		// create sections
-		final MasterContractEntity e = contractRepository.findById(resultRecord.getId()).get();
-		
-		//save a copy in history table
-		MasterContractHistoryDto contractHistoryDto = contractHistoryRepository.saveFrom(e);
-			
-		MasterContractHistoryEntity cEntity = contractHistoryRepository.findById(contractHistoryDto.getId()).get();
-		for (MasterSectionDto s : sections){
-			//s.setContract(record);
-			sectionHistoryRepository.saveFrom(s, cEntity);
-		} 
-				
-		// increment version
-		record.setVersion("" + (Integer.parseInt(record.getVersion())+1));
-		contractRepository.saveFrom(record);
-		
-		
-		
-		//List<SectionEntity> contractSections = contractRepository.findSectionsByContract(e);
-		List<Integer> newSectionIds = new ArrayList<Integer>(); 
-		int newId;
-		for (MasterSectionDto s : sections){
-			s.setContract(record);
-			newId =  sectionRepository.saveFrom(s).getId();
-			newSectionIds.add(newId);
-		}
-		
-		List<Integer> prevSectionIds = contractRepository.findSectionsIdsByContract(e);
-		List<Integer> differences = new ArrayList<>(prevSectionIds);
-		differences.removeAll(newSectionIds);
-		for (Integer i: differences) {
-			sectionRepository.remove(i);
-		}
-		
-		
-		return RestResponse.result(resultRecord);
-	}
-	
-	@Override
-	public RestResponse<MasterContractDraftDto> updateDraft(int id, MasterContractDraftDto record, BindingResult validationResult) {
-	
-		
-		final HelpdeskAccountDto account = helpdeskAccountRepo.findOneByEmail(this.currentUserName()).get().toDto();
+        if (result == null) {
+            return RestResponse.notFound();
+        }
 
-		record.setAccount(account);
-		List<MasterSectionDraftDto> sections = record.getSections();
-		record.setSections(null);
-		MasterContractDraftDto resultRecord = contractDraftRepository.saveFrom(record);
-		resultRecord = contractDraftRepository.saveFrom(record);
+        return RestResponse.result(result);
+    }
 
-		// create sections
-		final MasterContractDraftEntity e = contractDraftRepository.findById(resultRecord.getId()).get();
-		record.setId(e.getId());
-		
-		
-		
-		//List<SectionEntity> contractSections = contractRepository.findSectionsByContract(e);
-		List<Integer> newSectionIds = new ArrayList<Integer>(); 
-		int newId;
-		for (MasterSectionDraftDto s : sections){
-			s.setContract(record);
-			newId =  sectionDraftRepository.saveFrom(s).getId();
-			newSectionIds.add(newId);
-		}
-		
-		List<Integer> prevSectionIds = contractDraftRepository.findSectionsIdsByContract(e);
-		List<Integer> differences = new ArrayList<>(prevSectionIds);
-		differences.removeAll(newSectionIds);
-		for (Integer i: differences) {
-			sectionDraftRepository.remove(i);
-		}
-		
-		
-		return RestResponse.result(resultRecord);
-	}
+    @Override
+    public RestResponse<MasterContractDto> createDraft(MasterContractCommandDto command, BindingResult validationResult) {
+        try {
+            command.setUserId(this.currentUserId());
 
-	@Override
-	public RestResponse<Void> delete(int id) {
-		final MasterContractEntity ce = contractRepository.findById(id).orElse(null);
+            if (validationResult.hasErrors()) {
+                return RestResponse.invalid(validationResult.getFieldErrors());
+            }
 
-		if (ce == null) {
-			return RestResponse.notFound();
-		}
-		List<MasterSectionEntity> sections = contractRepository.findSectionsByContract(ce);
-		for (MasterSectionEntity s : sections){
-			sectionRepository.remove(s.getId());
-		}
-		// remove all versions
-		List <MasterContractHistoryEntity> historyCeList = contractHistoryRepository.findContractVersions(ce.getParentId());
-		for (MasterContractHistoryEntity h : historyCeList) {
+            final MasterContractDto result = this.masterService.updateDraft(command);
 
-			List<MasterSectionHistoryEntity> sectionsHistory = contractHistoryRepository.findSectionsByContract(h);
-			for (MasterSectionHistoryEntity s : sectionsHistory){
-				sectionHistoryRepository.remove(s.getId());
-			}
-			contractHistoryRepository.remove(h.getId());
-		}
+            return RestResponse.result(result);
+        } catch (final ApplicationException ex) {
+            return RestResponse.error(ex.getCode(), ex.getMessage());
+        }
+    }
 
-		
-		contractRepository.remove(id);
+    @Override
+    public RestResponse<MasterContractDto> updateDraft(int id, MasterContractCommandDto command, BindingResult validationResult) {
+        try {
+            command.setId(id);
+            command.setUserId(this.currentUserId());
 
-		return RestResponse.success();
-	}
+            if (validationResult.hasErrors()) {
+                return RestResponse.invalid(validationResult.getFieldErrors());
+            }
 
-	@Override
-	public RestResponse<Void> deleteDraft(int id) {
-		final MasterContractDraftEntity ce = contractDraftRepository.findById(id).orElse(null);
+            final MasterContractDto result = this.masterService.updateDraft(command);
 
-		if (ce == null) {
-			return RestResponse.notFound();
-		}
-		List<MasterSectionDraftEntity> sections = contractDraftRepository.findSectionsByContract(ce);
-		for (MasterSectionDraftEntity s : sections){
-			sectionDraftRepository.remove(s.getId());
-		}
-	
-		contractDraftRepository.remove(id);
+            return RestResponse.result(result);
+        } catch (final ApplicationException ex) {
+            return RestResponse.error(ex.getCode(), ex.getMessage());
+        }
+    }
 
-		return RestResponse.success();
-	}
+    @Override
+    public RestResponse<MasterContractDto> deleteDraft(int id) {
+        try {
+            final MasterContractDto result = masterService.deleteDraft(id);
 
-	
-	@Override
-	public RestResponse<Void> updateState(int id, String state) {
-		state =  state.replace("=", "");
-		if (state.equals("DRAFT")) {
-			final MasterContractEntity ce = contractRepository.findById(id).orElse(null);
+            return RestResponse.result(result);
+        } catch (final ApplicationException ex) {
+            return RestResponse.error(ex.getCode(), ex.getMessage());
+        }
+    }
 
-			if (ce == null) {
-				return RestResponse.notFound();
-			}
-			
-			//save contract in draft
-			MasterContractDraftDto cDto = contractDraftRepository.saveFrom(ce.toDto());
-			
-			// save sections
-			List<MasterSectionEntity> sections = contractRepository.findSectionsByContract(ce);
-			for (MasterSectionEntity s : sections){
-				sectionDraftRepository.saveFrom(s.toDto(), contractDraftRepository.findById(cDto.getId()).get() );
-			} 
-			
-			//remove from published
-			this.delete(id);
-			
-			return RestResponse.success();
-		}
-		else {
-			
-			final MasterContractDraftEntity ce = contractDraftRepository.findById(id).orElse(null);
-			if (ce == null) {
-				return RestResponse.notFound();
-			}
-			final MasterContractEntity ceOld = contractRepository.findById(id).orElse(null);
-			
-			ce.setState(state);
-			
-			// increment version
-			ce.setVersion("" + (Integer.parseInt(ce.getVersion())+1));
-			
-			//save contract in history table
-			MasterContractHistoryDto contractHistoryDto = contractHistoryRepository.saveFrom(ce);
+    @Override
+    public RestResponse<MasterContractDto> publishDraft(int id) {
+        try {
+            final MasterContractDto result = this.masterService.publishDraft(id);
 
-			
-			
-			//save contract in published
-			MasterContractDto cDto = contractRepository.saveFrom(ce.toDto());
-			// save sections
-			
-			List<MasterSectionDraftEntity> sections = contractRepository.findDraftSectionsByContract(ce);
-			for (MasterSectionDraftEntity s : sections){
-				sectionRepository.saveFrom(s.toDto(), contractRepository.findById(cDto.getId()).get() );
-			} 
-			
-			
-			MasterContractHistoryEntity contractHistoryEntity = contractHistoryRepository.findById(contractHistoryDto.getId()).get();
-			//save sections in history table
-			for (MasterSectionDraftEntity s : sections){
-				//s.setContract(record);
-				sectionHistoryRepository.saveFrom(s.toDto(), contractHistoryEntity);
-			} 
-			//remove from drafts
-			this.deleteDraft(id);
-			return RestResponse.success();
-		}
-	}
-	// TODO: Create separate service
+            return RestResponse.result(result);
+        } catch (final ApplicationException ex) {
+            return RestResponse.error(ex.getCode(), ex.getMessage());
+        }
+    }
+
 }
